03bed94f85c522041d4431019e5d6646d0da1f7a
Leo100k <lemail2004@mail.ru>
2023-10-13 12:14:41 +0300

    1.3. Вызов метода с аргументами. [#166086 #452240]

--- src/main/java/ru/job4/oop/Jukebox.java
+++ src/main/java/ru/job4/oop/Jukebox.java
@@ -6,8 +6,7 @@ public class Jukebox {
             case 1 -> System.out.println("Пусть бегут неуклюже");
             case 2 -> System.out.println("Спокойной ночи");
             default -> System.out.println("Песня не найдена");
-        };
-
+        }
     }
 
     public static void main(String[] args) {

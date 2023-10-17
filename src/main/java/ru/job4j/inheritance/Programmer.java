f091cb79830bf37058f4afa27bc51235ce6aff80
Leo100k <lemail2004@mail.ru>
2023-10-17 14:02:50 +0300

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class Programmer extends Profession {
+    private String programLang;
+
+    public Programmer(String programLang, int experience, boolean degree) {
+        super(degree);
+        this.programLang = programLang;
+    }
+}

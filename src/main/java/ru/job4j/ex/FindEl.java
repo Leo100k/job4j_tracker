94f76965bf84c3d0f5be57b4c25b1c0223cdcb52
Leo100k <lemail2004@mail.ru>
2023-10-29 16:08:27 +0300

    0.4. Пользовательские исключения. [#219367 #455902]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,8 +4,9 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (key == value[i]) {
+            if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         throw new ElementNotFoundException("Element Not Found");
@@ -19,4 +20,5 @@ public class FindEl {
             e.printStackTrace();
         }
     }
+
 }

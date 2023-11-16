61cc06bd9ba5fbefe2c147cf6854779ab7a87c9c
Leo100k <lemail2004@mail.ru>
2023-11-16 09:42:27 +0300

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -5,7 +5,8 @@ import java.util.function.*;
 
 public class FunctionalInterfaces {
     public static void main(String[] args) {
-        BiConsumer<Integer, String> biCon = (s, s1) -> System.out.println(s + " " + s1);
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (k, v) -> map.put(k, v);
         biCon.accept(1, "one");
         biCon.accept(2, "two");
         biCon.accept(3, "three");
@@ -14,25 +15,20 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-
         BiPredicate<Integer, String> biPred = (s, i) -> s % 2 == 0 || i.length() == 4;
         for (Integer i : map.keySet()) {
-            System.out.println("Результат : " + biPred.test(i, map.get(i)));
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("Замена условия в IF на Predicate" + map.get(i));
+            }
         }
 
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
 
-        Consumer<String> con = (s) -> System.out.println("Замена for each на Consumer" + s);
-        con.accept(String.valueOf(sup.get()));
+        Consumer<String> con = (s) -> System.out.println("Замена for each на Consumer + Function" + s);
         Function<String, String> func = s -> s.toUpperCase();
-        System.out.println("Заглавные символы: " + func.apply(String.valueOf(sup.get())));
+        List<String> strings = sup.get();
+        for (String s : strings) {
+            con.accept(func.apply(s));
+        }
     }
 }

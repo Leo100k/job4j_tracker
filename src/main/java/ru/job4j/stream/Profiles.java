b4462eebcce2a90589928dede2fd9d683cf25922
Leo100k <lemail2004@mail.ru>
2023-11-24 15:27:22 +0300

    4. Перенос методов в Stream API [#279131 #461944]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,7 +8,7 @@ public class Profiles {
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
-                .map(pr -> pr.getAddress())
+                .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
@@ -17,8 +17,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(pr -> pr.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
-
     }
 }
